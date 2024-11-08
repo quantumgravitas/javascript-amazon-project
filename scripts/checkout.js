378c7e66a032e0e21547270998e9930a7fde7d75
quantumgravitas <mayurgodase26@gmail.com>
2024-11-08 11:40:38 +0530

    deleting cartHTML when clicking the delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem)=>{
       }
   })
    cartSummaryHTML+= 
-   ` <div class="cart-item-container">
+   ` <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -97,7 +97,9 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((link)=>{
   
   link.addEventListener('click',()=>{
      const{productId}=link.dataset ;
+
      removeFromCart(productId);
-     console.log(cart); 
+      const container= document.querySelector(`.js-cart-item-container-${productId}`);
+       container.remove();
   })
 })
