9b752cdaf72d5cd6eeb2a3a5a01cfa47e5a087f0
quantumgravitas <mayurgodase26@gmail.com>
2024-11-08 11:17:26 +0530

    delete products from cart when clicking the delete link

--- data/cart.js
+++ data/cart.js
@@ -51,3 +51,16 @@ export  let cart=[
       })
       }
 }
+
+ export function removeFromCart(productId)
+{
+   const newCart=[];
+
+   cart.forEach((cartItem)=>{
+     if(cartItem.productId!==productId)
+     {
+        newCart.push(cartItem);
+     }
+   })
+   cart=newCart ;
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { currencyFormat } from './utils/money.js';
 
@@ -36,7 +36,7 @@ cart.forEach((cartItem)=>{
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-quantity-link" data-product-id=${matchingProduct.id}>
               Delete
             </span>
           </div>
@@ -92,3 +92,12 @@ cart.forEach((cartItem)=>{
 })
 
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-quantity-link').forEach((link)=>{
+  
+  link.addEventListener('click',()=>{
+     const{productId}=link.dataset ;
+     removeFromCart(productId);
+     console.log(cart); 
+  })
+})
