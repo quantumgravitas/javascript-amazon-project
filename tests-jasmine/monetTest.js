3a57f7e2ff262990e380bc20fe9a72d93a2f99e1
quantumgravitas <mayurgodase26@gmail.com>
2024-11-12 20:35:57 +0530

    Remove Default tests and add moneytests

---
+++ tests-jasmine/monetTest.js
@@ -0,0 +1,19 @@
+import {currencyFormat} from '../scripts/utils/money.js';
+
+describe('Test Suite: Format Currency',()=>{
+  it('Converting Cents into dollars',()=>{
+    expect(currencyFormat(2095)).toEqual('20.95');
+  });
+
+  it('Works with zero',()=>{
+    expect(currencyFormat(0)).toEqual('0.00');
+  });
+
+  it('Rounds up to the nearest Cents',()=>{
+    expect(currencyFormat(2000.5)).toEqual('20.01');
+  });
+
+  it('Rounds up to the nearest Cents less than 5',()=>{
+    expect(currencyFormat(2000.4)).toEqual('20.00');
+  });
+})
