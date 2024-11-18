9b3b093ca26ae38ace0e887a578e243ac72a4e54
quantumgravitas <mayurgodase26@gmail.com>
2024-11-18 08:47:14 +0530

    created automated tests for remove from Cart function

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,5 @@
-import {addToCart,cart,loadFromStorage} from '../../data/cart.js' ;
+import {addToCart,cart,loadFromStorage,removeFromCart} from '../../data/cart.js' ;
+import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
 describe('test Suite: Add to Cart',()=>{
   const productId1='e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   beforeEach(()=>{
@@ -63,4 +64,63 @@ describe('test Suite: Add to Cart',()=>{
     }])); 
    });
      
+})
+
+describe('Test Suite:Remove From Cart',()=>{
+  const productId1="e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2="15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  const productId3="83d4ca15-0f35-48f5-b7a3-1ea210004f2e"; 
+  beforeEach(()=>{
+    spyOn(localStorage,'setItem');
+    spyOn(localStorage,'getItem').and.callFake(()=>{
+      return  JSON.stringify([{
+        productId: productId1 ,
+        quantity: 2,
+        deliveryOptionId: '1'
+      },
+      {
+        productId:productId2 ,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]);
+     
+
+    });
+  })
+  it('Remove ProductId that is in the cart',()=>{
+     loadFromStorage();
+     removeFromCart(productId1);
+     expect(cart.length).toEqual(1);
+     removeFromCart(cart[0].productId);
+     expect(cart.length).toEqual(0);
+
+     expect(localStorage.setItem).toHaveBeenCalledTimes(2);
+
+     expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([
+      {
+        productId:productId2 ,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]));
+      
+      expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([]));
+  });
+
+  it('Remove ProductId that is not in the cart',()=>{
+    loadFromStorage();
+    removeFromCart(productId3);
+    expect(cart.length).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+      productId: productId1 ,
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId:productId2 ,
+      quantity: 1,
+      deliveryOptionId: '2'
+    }]));
+
+  });
 })
