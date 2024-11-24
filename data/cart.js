6d8399471db91b2b058f68a169ab9ce09cef42dd
quantumgravitas <mayurgodase26@gmail.com>
2024-11-24 19:04:07 +0530

    learn promises and convert  code to promises

--- data/cart.js
+++ data/cart.js
@@ -241,4 +241,16 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     matchingCartItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
-     
+   
+export function loadCart(fun)
+{
+  const xhr=new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+     console.log(xhr.response);
+     fun();
+  })
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
+}
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,12 +3,56 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import '../data/cart-class.js';
 import {loadProducts} from '../data/products.js';
+import { loadCart } from "../data/cart.js";
 //import '../data/backend-practice.js';
 //import '../data/car.js';
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve();
+    })
+  }),
 
+  new Promise((resolve)=>{
+    loadCart(()=>{
+    resolve();
+    })
+  })
+]).then(()=>{
   renderOrderSummary();
   renderPaymentSummary();
 })
+/* 
+new Promise((resolve)=>{
+   loadProducts(()=>{
+     resolve();
+   })
+
+}).then(()=>{
+    return new Promise((resolve)=>{
+      loadCart(()=>{
+      resolve();
+      })
+    })
+
+}).then(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+*/
+    
+
+/*
+loadProducts(()=>{
+  loadCart(()=>{
+      renderOrderSummary();
+      renderPaymentSummary();
+  })
+})
+  */
+
+  
+  
+
   
