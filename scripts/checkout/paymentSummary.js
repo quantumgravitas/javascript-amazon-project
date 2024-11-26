5c89d18ebec1209c804710ac07aecce91d7774fb
quantumgravitas <mayurgodase26@gmail.com>
2024-11-26 11:21:42 +0530

    create order using backend requests

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,7 +2,7 @@ import{cart,calculateCartQuantity} from '../../data/cart.js'
 import {getProduct} from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryoptions.js';
 import {currencyFormat} from '../utils/money.js';
-
+import { addOrder } from '../../data/orders.js';
 export function renderPaymentSummary()
 { 
   let productPriceCents=0;
@@ -52,11 +52,29 @@ export function renderPaymentSummary()
           <div class="payment-summary-money js-payment-summary-money-total">$${currencyFormat(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
           Place your order
         </button>
        `;
    document.querySelector('.js-payment-summary').innerHTML=paymentSummaryHTML;    
 
-   
+   document.querySelector('.js-place-order').addEventListener('click',async ()=>{
+   try{
+      const response= await fetch('https://supersimplebackend.dev/orders',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+        },
+      body:JSON.stringify({
+        cart:cart
+        })
+    });
+     const order= await response.json();
+     addOrder(order);
+   }catch(error)
+   {
+      console.log('Unexpected Error: please try again later');
+   }  
+    window.location.href='orders.html';
+   });
 }
